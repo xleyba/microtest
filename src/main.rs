a747cc7ce4ce7bcf5cb45064b7326dbc1585f53d
Javier L <135663+xleyba@users.noreply.github.com>
2020-04-27 16:15:43 +0200

    added server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+        .bind("127.0.0.1:5000")?
+        .run()
+        .await
 }
